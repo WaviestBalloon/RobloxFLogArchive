07aa435a561b673b55b487d6a1c016dee9fa9c60
WaviestBalloon <31796727+WaviestBalloon@users.noreply.github.com>
2023-09-14 00:44:58 +0100

    Remove the replace string in diff output and clean up imports

--- src/utils/Diff.ts
+++ src/utils/Diff.ts
@@ -1,20 +1,18 @@
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export async function generateDiff(newArray: Array<string>, oldArray: Array<string>): Promise<Array<string>> {
 	let diffResult = [];
 	for (let i = 0; i < newArray.length; i++) {
 		if (!oldArray.includes(newArray[i])) {
 			console.log(newArray[i])
-			diffResult.push(`+ ${newArray[i].replace("[FLog::Output] ", "")}`);
+			diffResult.push(`+ ${newArray[i]}`);
 		}
 	}
 	for (let i = 0; i < oldArray.length; i++) {
 		if (!newArray.includes(oldArray[i])) {
 			console.log(oldArray[i])
-			diffResult.push(`- ${oldArray[i].replace("[FLog::Output] ", "")}`);
+			diffResult.push(`- ${oldArray[i]}`);
 		}
 	}
 	
